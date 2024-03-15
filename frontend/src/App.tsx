a26e8d81c38a6abd51cd510ac92069d1ee3a86e5
kshetritej <tejghartikshetri@gmail.com>
2024-03-15 20:51:39 +0545

    inital commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import AddTodo from "@/components/AddTodo";
+function App() {
+  return (
+    <>
+      <AddTodo/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/AddTodo.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+const AddTodo = () => {
+  return (
+    <div>
+     <Button> Add Todo </Button> 
+    </div>
+  )
+}
+
+export default AddTodo;
