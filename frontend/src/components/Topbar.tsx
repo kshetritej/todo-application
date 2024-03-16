fd92d3f7047ec00766158b6f64d32b4f721c59da
kshetritej <tejghartikshetri@gmail.com>
2024-03-16 12:02:16 +0545

    update(topbar): add icon

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,11 +1,11 @@
+import { Inbox } from "lucide-react";
 import   AddTodo from "./AddTodo";
 const Topbar = () => {
   return (
     <div className="flex border-b-2 justify-between p-2 ">
       <div>
-        <h1 className="text-2xl font-bold"> Todo </h1>
+        <h1 className="text-2xl flex gap-2 font-bold align-items-center justify-center"> <span className="py-1"><Inbox/> </span>Todo </h1>
       </div>
-
       <div>
         <AddTodo />
       </div>
